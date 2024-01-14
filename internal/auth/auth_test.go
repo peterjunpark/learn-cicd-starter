85d6a6acb6be759a778a74b9e0c856f391167fa1
Peter Jun Park <park@hey.com>
2024-01-13 19:27:36 -0800

    add test to workflow (this is supposed to fail all tests!)

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,16 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestAuthFail(t *testing.T) {
+	header := http.Header(map[string][]string{
+		"Authorization": {"asuh", "dud"},
+	})
+	res, err := GetAPIKey(header)
+	if err != nil {
+		t.Error("there should be an error", res, err)
+	}
+}
